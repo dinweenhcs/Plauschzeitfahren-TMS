292682710c08f4dde0db8236d299873b9963f40e
mrjeki <git@schneewind.ch>
2016-05-12 21:38:44 +0200

    Reorganisation of code

--- PlauschzeitfahrenTMS/MainViewController.cs
+++ PlauschzeitfahrenTMS/MainViewController.cs
@@ -1,14 +1,52 @@
+#region "Internal Libraries"
 using Foundation;
-using System;
-using System.CodeDom.Compiler;
 using UIKit;
+using System;
+#endregion
+
+#region "Importet Libraries"
+#endregion
+
+#region "Own Classes"
+#endregion
 
 namespace PlauschzeitfahrenTMS
 {
 	partial class MainViewController : UIViewController
 	{
+		#region "### Properties #############################################"
+		#endregion
+
+		#region "### Constructors #############################################"
 		public MainViewController (IntPtr handle) : base (handle)
 		{
 		}
+		#endregion
+
+		#region "### Deconstructors #############################################"
+		#endregion
+
+		#region "### Viewmethods #############################################"
+		public override void ViewDidLoad ()
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+		#endregion
+
+		#region "### Private Methods #############################################"
+		#endregion
+
+
+		#region "### Public Methods #############################################"
+		#endregion
 	}
 }
+
+
+
+
--- PlauschzeitfahrenTMS/TimeViewController.cs
+++ PlauschzeitfahrenTMS/TimeViewController.cs
@@ -1,30 +1,73 @@
+#region "Internal Libraries"
 using Foundation;
-using System;
-using System.CodeDom.Compiler;
 using UIKit;
+using System;
+using System.Timers;
+#endregion
+
+#region "Importet Libraries"
+#endregion
+
+#region "Own Classes"
+#endregion
 
 namespace PlauschzeitfahrenTMS
 {
 	partial class TimeViewController : UIViewController
 	{
+
+		#region "### Properties #############################################"
+		private Timer _time;
+		#endregion
+
+		#region "### Constructors #############################################"
 		public TimeViewController (IntPtr handle) : base (handle)
 		{
 		}
+		#endregion
 
+		#region "### Deconstructors #############################################"
+		#endregion
+
+		#region "### Viewmethods #############################################"
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
 
-			Console.WriteLine ("ViewDidLoad");
+			this.labTime.Text = "ViewDidLoad";	
+			this.labGetTime.Text = "---";	
+
+			this._time = new Timer();
+			this._time.Elapsed += (object sender, System.Timers.ElapsedEventArgs e) => {
+				//Console.WriteLine("-> {0}", e.SignalTime);
+				InvokeOnMainThread (delegate {   
+					labTime.Text = e.SignalTime.ToLongTimeString();
+				});
+				//RunOnUiThread(()=>this.labTime.Text = "A");
+			};
+			this._time.Interval = 1000;
+			this._time.AutoReset = true;
+			this._time.Enabled = true;
+
+
+			this.btnGetTime.TouchUpInside += (object sender, EventArgs e) => {
+				this.labGetTime.Text = this.labTime.Text;
+			};
 		}
-
-
+			
 		public override void DidReceiveMemoryWarning ()
 		{
 			base.DidReceiveMemoryWarning ();
 			// Release any cached data, images, etc that aren't in use.
 		}
+		#endregion
 
+		#region "### Private Methods #############################################"
+		#endregion
+
+
+		#region "### Public Methods #############################################"
+		#endregion
 	}
 }
